acff1febf5cc137d1fa1e63bd1be59f2edd58c0c
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-05-22 10:19:57 -0600

    first official commit of the bootcamp!

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
+// how nice that we let all our variables be variables
